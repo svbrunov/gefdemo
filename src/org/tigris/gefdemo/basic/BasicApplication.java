d2e373133ca56abb7e90edcb1ad6ff96003afba9
Bob Tarling <BobTarling>
2004-06-07 14:29:48 +0000

    Don't bind delete key

--- src/org/tigris/gefdemo/basic/BasicApplication.java
+++ src/org/tigris/gefdemo/basic/BasicApplication.java
@@ -13,60 +13,47 @@ import org.tigris.gef.graph.presentation.*;
 
 public class BasicApplication {
 
-  ////////////////////////////////////////////////////////////////
-  // instance variables
+    private JGraphFrame graphFrame;
 
-  protected JGraphFrame _jgf;
+    public BasicApplication() {
+        // init localizer and resourceloader
+        Localizer.addResource("GefBase","org.tigris.gef.base.BaseResourceBundle");
+        Localizer.addResource("GefPres","org.tigris.gef.presentation.PresentationResourceBundle");
+        Localizer.addLocale(Locale.getDefault());
+        Localizer.switchCurrentLocale(Locale.getDefault());
+        ResourceLoader.addResourceExtension("gif");
+        ResourceLoader.addResourceLocation("/org/tigris/gef/Images");
+        graphFrame = new JGraphFrame();
+        graphFrame.addWindowListener(
+            new WindowAdapter() {
+                public void windowClosing(WindowEvent event) {
+                  graphFrame.dispose();
+                }
+                public void windowClosed(WindowEvent event) {
+                  System.exit(0);
+                }
+            }
+        );
 
-  ////////////////////////////////////////////////////////////////
-  // constructors
-
-  public BasicApplication() {
-	  // init localizer and resourceloader
-	  Localizer.addResource("GefBase","org.tigris.gef.base.BaseResourceBundle");
-	  Localizer.addResource("GefPres","org.tigris.gef.presentation.PresentationResourceBundle");
-	  Localizer.addLocale(Locale.getDefault());
-	  Localizer.switchCurrentLocale(Locale.getDefault());
-	  ResourceLoader.addResourceExtension("gif");
-	  ResourceLoader.addResourceLocation("/org/tigris/gef/Images");
-    _jgf = new JGraphFrame();
-    _jgf.addWindowListener(new WindowAdapter() {
-        public void windowClosing(WindowEvent event) {
-          _jgf.dispose();
+        graphFrame.setToolBar(new SamplePalette()); //needs-more-work
+    
+        // make the delete key remove elements from the underlying GraphModel
+        //_jgf.getGraph().bindKey(new CmdDispose(), KeyEvent.VK_DELETE, 0);
+    
+        graphFrame.setBounds(10, 10, 300, 200);
+        graphFrame.setVisible(true);
+        LayerManager lm =  graphFrame.getGraph().getEditor().getLayerManager();
+        LayerPerspective lay = (LayerPerspective) lm.getActiveLayer();
+        lay.addNodeTypeRegion(SampleNode.class, new Rectangle(10, 10, 200, 200));
+        DefaultGraphModel dgm = (DefaultGraphModel) graphFrame.getGraphModel();
+        for (int i = 0; i < 1; i++) {
+            SampleNode sn = new SampleNode();
+            sn.initialize(null);
+            dgm.addNode(sn);
         }
-        public void windowClosed(WindowEvent event) {
-          System.exit(0);
-        }
-      });
-
-    _jgf.setToolBar(new SamplePalette()); //needs-more-work
-
-    // make the delete key remove elements from the underlying GraphModel
-    _jgf.getGraph().bindKey(new CmdDispose(), KeyEvent.VK_DELETE, 0);
-
-    _jgf.setBounds(10, 10, 300, 200);
-    _jgf.setVisible(true);
-    LayerManager lm =  _jgf.getGraph().getEditor().getLayerManager();
-    LayerPerspective lay = (LayerPerspective) lm.getActiveLayer();
-    lay.addNodeTypeRegion(SampleNode.class, new Rectangle(10, 10, 200, 200));
-    //lay.addNodeTypeRegion(SampleNode2.class, new Rectangle(250, 10, 200, 200));
-    DefaultGraphModel dgm = (DefaultGraphModel) _jgf.getGraphModel();
-    for (int i = 0; i < 1; i++) {
-      SampleNode sn = new SampleNode();
-      sn.initialize(null);
-      dgm.addNode(sn);
-      //      SampleNode2 sn2 = new SampleNode2();
-      //      sn2.initialize(null);
-      //      dgm.addNode(sn2);
     }
-  }
 
-  ////////////////////////////////////////////////////////////////
-  // main
-
-  public static void main(String args[]) {
-    BasicApplication demo = new BasicApplication();
-  }
-
-} /* end class BasicApplication */
-
+    public static void main(String args[]) {
+        BasicApplication demo = new BasicApplication();
+    }
+}
